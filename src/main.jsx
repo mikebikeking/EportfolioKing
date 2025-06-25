b89bb8cc1b6bdbad7ec29622ad3ab77f11107810
Michael <mikebikeking@gmail.com>
2025-06-25 07:05:58 -0500

    remove bad line

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import { Analytics } from "@vercel/analytics/next"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
